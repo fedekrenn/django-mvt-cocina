fad31a791e988f24e64200ba1645cd35212d9a21
Fede Krenn <fedekrenn@gmail.com>
2023-07-25 16:25:19 -0300

    Se termina el renderizado de los datos en la db

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cocinero, Receta, Restaurante
 
 # Register your models here.
+admin.site.register(Cocinero)
+admin.site.register(Receta)
+admin.site.register(Restaurante)
