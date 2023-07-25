b57c70f9a91c799b71059597b54056df1d2f6f2f
Fede Krenn <fedekrenn@gmail.com>
2023-07-25 15:49:23 -0300

    Sección main html lista

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Receta(models.Model):
+    nombre = models.CharField(max_length=50)
+    ingredientes = models.CharField(max_length=200)
+    tiempo = models.IntegerField()
+    dificultad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Cocinero(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    especialidad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+
+
+class Restaurante(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
