056f2c60812b29a0c5ec2f53eb53a13c73ecb03f
Fede Krenn <fedekrenn@gmail.com>
2023-08-10 19:12:27 -0300

    Se añade funcionalidad completa de CRUD

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -49,6 +49,33 @@ def delete_receta(request, id):
     return render(request, "aplicacion/confirmacion-eliminado.html")
 
 
+def edit_receta(request, id):
+    receta = Receta.objects.get(id=id)
+    if request.method == "POST":
+        form = RecetaForm(request.POST)
+        if form.is_valid():
+            receta.nombre = form.cleaned_data["nombre"]
+            receta.ingredientes = form.cleaned_data["ingredientes"]
+            receta.tiempo = form.cleaned_data["tiempo"]
+            receta.dificultad = form.cleaned_data["dificultad"]
+            receta.save()
+
+            return render(request, "aplicacion/confirmacion-guardado.html")
+    else:
+        form = RecetaForm(
+            initial={
+                "nombre": receta.nombre,
+                "ingredientes": receta.ingredientes,
+                "tiempo": receta.tiempo,
+                "dificultad": receta.dificultad,
+            }
+        )
+
+    ctx = {"recetas": Receta.objects.all(), "form": form}
+
+    return render(request, "aplicacion/editar.html", ctx)
+
+
 def cocineros(request):
     if request.method == "POST":
         form = CocineroForm(request.POST)
@@ -73,6 +100,33 @@ def cocineros(request):
 def delete_cocinero(request, id):
     Cocinero.objects.get(id=id).delete()
     return render(request, "aplicacion/confirmacion-eliminado.html")
+
+
+def edit_cocinero(request, id):
+    cocinero = Cocinero.objects.get(id=id)
+    if request.method == "POST":
+        form = CocineroForm(request.POST)
+        if form.is_valid():
+            cocinero.nombre = form.cleaned_data["nombre"]
+            cocinero.apellido = form.cleaned_data["apellido"]
+            cocinero.edad = form.cleaned_data["edad"]
+            cocinero.especialidad = form.cleaned_data["especialidad"]
+            cocinero.save()
+
+            return render(request, "aplicacion/confirmacion-guardado.html")
+    else:
+        form = CocineroForm(
+            initial={
+                "nombre": cocinero.nombre,
+                "apellido": cocinero.apellido,
+                "edad": cocinero.edad,
+                "especialidad": cocinero.especialidad,
+            }
+        )
+
+    ctx = {"cocineros": Cocinero.objects.all(), "form": form}
+
+    return render(request, "aplicacion/editar.html", ctx)
 
 
 def restaurantes(request):
@@ -102,3 +156,30 @@ def restaurantes(request):
 def delete_restaurante(request, id):
     Restaurante.objects.get(id=id).delete()
     return render(request, "aplicacion/confirmacion-eliminado.html")
+
+
+def edit_restaurante(request, id):
+    restaurante = Restaurante.objects.get(id=id)
+    if request.method == "POST":
+        form = RestauranteForm(request.POST)
+        if form.is_valid():
+            restaurante.nombre = form.cleaned_data["nombre"]
+            restaurante.direccion = form.cleaned_data["direccion"]
+            restaurante.telefono = form.cleaned_data["telefono"]
+            restaurante.categoria = form.cleaned_data["categoria"]
+            restaurante.save()
+
+            return render(request, "aplicacion/confirmacion-guardado.html")
+    else:
+        form = RestauranteForm(
+            initial={
+                "nombre": restaurante.nombre,
+                "direccion": restaurante.direccion,
+                "telefono": restaurante.telefono,
+                "categoria": restaurante.categoria,
+            }
+        )
+
+    ctx = {"restaurantes": Restaurante.objects.all(), "form": form}
+
+    return render(request, "aplicacion/editar.html", ctx)
