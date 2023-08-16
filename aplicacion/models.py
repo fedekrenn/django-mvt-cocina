1b4ed25c7e98e234a9dc5787bcd4d40cec45af69
Fede Krenn <fedekrenn@gmail.com>
2023-08-16 00:04:31 -0300

    Se mejora funcionalidad de leer avatar de user

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Receta(models.Model):
     nombre = models.CharField(max_length=50)
     ingredientes = models.CharField(max_length=200)
@@ -90,4 +91,4 @@ class Avatar(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Avatar de {self.user.username}'
+        return f"{self.user} [{self.imagen}]"
