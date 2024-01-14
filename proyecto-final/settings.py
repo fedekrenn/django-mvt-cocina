314228387ace1cec4c6ce9a9badb3a1e69e73f67
Fede Krenn <fedekrenn@gmail.com>
2024-01-14 16:23:58 -0300

    Update allowed hosts and add requirements.txt and vercel.json

--- proyecto-final/settings.py
+++ proyecto-final/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "***REMOVED-SECRET-KEY***"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
