4a39dc410f520a62c28304b7566c152648b66177
Fede Krenn <fedekrenn@gmail.com>
2024-01-14 17:51:11 -0300

    Update static file paths in settings.py and urls.py

--- proyecto-final/urls.py
+++ proyecto-final/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("aplicacion.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
