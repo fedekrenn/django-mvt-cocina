5e68e51e19e698c3397321d13c6787dd67640833
Fede Krenn <fedekrenn@gmail.com>
2024-01-14 17:51:11 -0300

    Update static file paths in settings.py and urls.py

--- proyecto-final/settings.py
+++ proyecto-final/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "aplicacion/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
