206d1f5c265927b4c072f128f1c2a8d79dcf47ca
Fede Krenn <fedekrenn@gmail.com>
2024-01-15 01:07:13 -0300

    Add build_files.sh and update static file configuration

--- proyecto-final/settings.py
+++ proyecto-final/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "aplicacion/static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- proyecto-final/urls.py
+++ proyecto-final/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("", include("aplicacion.urls")),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
